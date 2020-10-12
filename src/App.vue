e27c6f18695c0d27748b28a5a699ef74e0ddf75d
邱一航 <2061410379@qq.com>
2020-10-12 09:59:21 +0800

    111111

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+	<home></home>
+  </div>
+</template>
+
+<script>
+import home from './home/home.vue'
+export default {
+  name: 'app',
+  components: {
+      home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+body{
+	margin: 0;
+	padding: 0;
+}
+</style>
